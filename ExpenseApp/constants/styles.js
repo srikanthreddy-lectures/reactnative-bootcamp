1d0fdd253a6bf978c1bcfaa8cd3f64560d6b985e
srikanthreddy-lectures <83769934+srikanthreddy-lectures@users.noreply.github.com>
2023-03-25 15:41:07 +0530

    ExpenseApp

---
+++ ExpenseApp/constants/styles.js
@@ -0,0 +1,16 @@
+export const GlobalStyles = {
+  colors: {
+    primary50: '#e4d9fd',
+    primary100: '#c6affc',
+    primary200: '#a281f0',
+    primary400: '#5721d4',
+    primary500: '#1b3646',
+    primary700: '#b2c7c6',
+    primary800: '#200364',
+    accent500: '#f7bc0c',
+    error50: '#fcc4e4',
+    error500: '#9b095c',
+    gray500: '#39324a',
+    gray700: '#221c30',
+  },
+};
